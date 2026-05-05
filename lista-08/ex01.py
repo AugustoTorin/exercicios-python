97f4e83eaebc46ccc1799a74facfdc57e514d4d9
Augusto Mota Torin <augustotorin@gmail.com>
2026-05-05 16:09:32 -0300

    lista 08: exercício 01 - contagem de caracteres

---
+++ lista-08/ex01.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba uma string e mostre: Quantos caracteres ela possui (incluindo espaços)
+
+texto = 'Esse texto é apenas um teste para contar caracteres e espaços'
+    
+print(f'Este texto contém: {len(texto)} caracteres')
