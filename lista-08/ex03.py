8a5b05d8a61ba059508773c1c4fcb8b47d948ba1
Augusto Mota Torin <augustotorin@gmail.com>
2026-05-06 10:40:42 -0300

    lista 08: exercício 03 - identificador primeira e última letra

---
+++ lista-08/ex03.py
@@ -0,0 +1,6 @@
+# Receba uma palavra e exiba: A primeira letra, a última letra
+
+palavra = 'Aleatório'
+
+print(f'A primeira letra da palavra é {palavra[0]}')
+print(f'A última letra da palavra é {palavra[-1]}')
